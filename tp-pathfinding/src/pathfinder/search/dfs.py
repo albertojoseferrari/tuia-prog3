d1d1343f2abbeb374c6c95eb3834cbb7d2fbe055
lucia <luciamoroni15@gmail.com>
2026-04-08 13:18:26 -0300

    Implementación DFS

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -8,21 +8,36 @@ class DepthFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
         """Find path between two points in a grid using Depth First Search
-
         Args:
             grid (Grid): Grid of points
-
         Returns:
             Solution: Solution found
         """
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
-
         # Initialize expanded with the empty dictionary
         expanded = dict()
-
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = StackFrontier()
+        frontier.add(root)
+        if grid.objective_test(root.state):
+            return Solution(root, expanded)
+        
+        while True:
+            if frontier.is_empty():
+                return NoSolution(expanded)
+            n = frontier.remove()
+            if n.state in expanded:
+                continue
 
-        return NoSolution(expanded)
+            expanded[n.state] = True
+            for a in grid.actions(n.state):
+                ss = grid.result(n.state,a)
+                if ss not in expanded:
+                    nn = Node("",ss,n.cost+grid.individual_cost(n.state,a),n,a)
+                    if grid.objective_test(ss):
+                        return Solution(nn, expanded)
+                    frontier.add(nn)
+                    #expanded[ss] = True   # ← AGREGÁ ESTA LÍNEA
+
+        #return NoSolution(expanded)
